aeaa21b16482ee2eea1b5ba8435615c191ec04d3
Raymund Martinez <zhaqenl@gmail.com>
2018-11-21 16:27:20 +0800

    Add shortcut for qt5ct

--- apps.lisp
+++ apps.lisp
@@ -17,6 +17,8 @@
            #:oa-x
            #:oa
            #:a0
+
+           #:qt
            
            #:vbox))
 
@@ -24,35 +26,15 @@
 
 (defvar +screenshots-dir+ (mof:home ".screenshots"))
 
-(defun run-with-locale (locale &rest args)
-  "Run args with locale set to LOCALE"
-  (setf (getenv "LANG") locale)
-  (run/i `(,@(first args)))
-  (success))
-
-(defun run-with-nix-user (profile binary &rest args)
-  "Run binary under a separate profile"
-  (let ((bin (mof:home (mof:fmt ".baf/profiles/~A/bin" profile))))
-    (setf (getenv "PATH") (unix-namestring bin))
-    (run/i `(,binary ,@args))))
-
-(defun run-with-nix-system (binary &rest args)
-  "Run binary without user paths"
-  (setf (getenv "PATH") "/var/setuid-wrappers:/run/wrappers/bin:/run/current-system/sw/bin:/run/current-system/sw/sbin:/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin")
-  (run/i `(,binary ,@args)))
-
-(defun run-with-xdg (binary &rest args)
-  "Run binary under a custom XDG_DATA_DIRS path"
-  (setf (getenv "XDG_DATA_DIRS")
-        (uiop:native-namestring (mof:home ".local/share/mime")))
-  (run/i `(,binary ,@args)))
-
 (exporting-definitions
  (% s "sudo")
  (% e "emacsclient -nw")
  (% oa-x "xhost local:root")
  (% oa "docker run --rm -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix ebzzry/opera /usr/bin/opera --no-sandbox")
  (% a0 "xmodmap /home/zhaqenl/.Xmodmap"))
+
+(exporting-definitions
+ ($ "qt5ct" qt5ct qt))
 
 (exporting-definitions
  (defun vbox () (run-with-nix-system "VirtualBox")))
